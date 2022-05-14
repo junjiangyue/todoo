301a8f1d41a8bf1b6c0147ac4228766a15b9cd01
junjiangyue <76471982+junjiangyue@users.noreply.github.com>
2022-05-14 23:30:43 +0800

    left-nav+top-nav

--- src/components/public/LeftBar.vue
+++ src/components/public/LeftBar.vue
@@ -5,24 +5,96 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <section class="side-nav-wrapper">
-    侧边栏
-    <router-link to="/">我的一天</router-link>
-    <router-link to="/calendar">日历</router-link>
-    <router-link to="/todo-asset">待办集</router-link>
-    <router-link to="/statistic">数据统计</router-link>
-    <router-link to="/settings">设置</router-link>
+    <router-link to="/">
+      <div class="daily">
+        <el-row :gutter="20">
+          <el-col :span="6"><img class="icon-left" src="@/assets/icon/daily.png"/></el-col>
+            <el-col :span="18">
+              <h4>我的一天</h4>
+              <p class="body">Daily todo</p>
+            </el-col>
+        </el-row>
+      </div>
+    </router-link>
+    <router-link to="/calendar">
+      <div class="daily">
+        <el-row :gutter="20">
+            <el-col :span="6"><img class="icon-left" src="@/assets/icon/calendar.png"/></el-col>
+              <el-col :span="18">
+                <h4>日历</h4>
+                <p class="body">Calendar</p>
+              </el-col>
+          </el-row>
+        </div>
+    </router-link>
+    <router-link to="/todo-asset">
+      <div class="daily">
+        <el-row :gutter="20">
+            <el-col :span="6"><img class="icon-left" src="@/assets/icon/daily.png"/></el-col>
+              <el-col :span="18">
+                <h4>待办集</h4>
+                <p class="body">Todo Asset</p>
+              </el-col>
+          </el-row>
+      </div>
+    </router-link>
+    <router-link to="/statistic">
+      <div class="daily">
+      <el-row :gutter="20">
+          <el-col :span="6"><img class="icon-left" src="@/assets/icon/daily.png"/></el-col>
+            <el-col :span="18">
+              <h4>数据统计</h4>
+              <p class="body">Statistics</p>
+            </el-col>
+        </el-row>
+        </div>
+    </router-link>
+    <router-link to="/settings">
+      <div class="daily">
+      <el-row :gutter="20">
+          <el-col :span="6"><img class="icon-left" src="@/assets/icon/daily.png"/></el-col>
+            <el-col :span="18">
+              <h4>设置</h4>
+              <p class="body">Settings</p>
+            </el-col>
+        </el-row>
+        </div>
+    </router-link>
   </section>
 </template>
 
 <style scoped>
 .side-nav-wrapper {
   box-sizing: border-box;
-  border: 1px solid black;
+  border: 0px solid black;
   border-top: none;
   font-size: 1rem;
-  width: 16.2em;
+  width: 16.25em;
   display: flex;
+  padding-left: 3em;
   flex-direction: column;
   gap: 1em;
+  margin-top: 6em;
+}
+.router-link-active {
+  text-decoration: none;
+  background-color: #E5EFFF;
+  border-radius: 10px;
+
+}
+a{
+  text-decoration: none;
+  color: #000;
+}
+.daily{
+  display:flex;
+  align-items:center;
+  margin-top: 0em;
+  width: 211px;
+  height: 56px;
+  padding-left: 1.2em;
+}
+.icon-left{
+  margin-top: 0.5em;
 }
 </style>
--- src/components/public/TopNav.vue
+++ src/components/public/TopNav.vue
@@ -4,18 +4,84 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <header class="top-nav-wrapper">
-     顶栏
-     <router-link to="/login">注销</router-link>
+    <div>
+      <el-row>
+        <el-col :span="20">
+          <img class="icon-left" src="@/assets/icon/logo.png"/>
+        </el-col>
+        <el-col :span="4">
+          <div class="right">
+            <el-row>
+              <el-col :span="9">
+              <div class="help">
+                帮助
+                <img src="@/assets/icon/help-circle.png"/>
+              </div>
+              </el-col>
+              <el-col :span="9"><div class="daylight">
+                白天模式
+              </div></el-col>
+              <el-col :span="4"><img src="@/assets/icon/userpic.png"/></el-col>
+              
+            </el-row>
+          </div>
+        </el-col>
+     </el-row>
+     </div>
+     
   </header>
 </template>
 
 <style scoped>
 .top-nav-wrapper {
   box-sizing: border-box;
-  border: 1px solid #000;
+  border: 0px solid #000;
   font-size: 1rem;
   height: 4em;
+}
+.icon-left{
+  margin-left: 1em;
+  margin-top: 0.5em;
+}
+.router-link-active {
+  text-decoration: none;
+}
+a{
+  text-decoration: none;
+  color: #000;
+}
+.daylight{
+  color: #367BF5;
+  font-family: 'PingFang SC';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  letter-spacing: 0.75px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  /* White Base */
+  background: #FFFFFF;
+  /* Shadow Blue · 4dp */
+  box-shadow: 0px 4px 8px rgba(54, 123, 245, 0.16), 0px 2px 4px rgba(54, 123, 245, 0.24);
+  border-radius: 10px;
+  width: 6em;
+  height: 2.5em;
+
+}
+.right{
+  margin-top: 0.5em;
+  
+}
+.help{
   display: flex;
-  justify-content: space-around;
+  background: #F7F9FA;
+  border-radius: 10px;
+  /* Gray · 80% */
+  color: #5E6366;
+  align-items:center;
+  justify-content:center;
+  width: 6em;
+  height: 2.5em;
 }
 </style>
