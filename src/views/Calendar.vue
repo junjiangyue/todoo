901cb82ed723566e8c273cf4566b6919f5fb12f6
fwio <624835339@qq.com>
2022-05-30 20:37:04 +0800

    Simple static calendar.

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,11 +1,30 @@
-<script setup>
+<script setup lang="ts">
+import { NScrollbar } from 'naive-ui'
+import '@fullcalendar/core/vdom'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
+const calendarOptions = {
+  plugins: [ dayGridPlugin, interactionPlugin ],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    start: 'prev',
+    center: 'title',
+    end: 'next'
+  }
+}
 </script>
 
 <template>
-日历
+  <n-scrollbar
+    style="max-height: 40rem;">
+    <full-calendar
+      style="margin-right: 1em;"
+      :options="calendarOptions"/>
+  </n-scrollbar>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
